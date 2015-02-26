a88e8040f2015eb74490147371d32c457840f1a3
caesarfeta <adamtavares@gmail.com>
2015-02-26 14:35:17 -0500

    Have to remember white-space conventions.

--- app/views/PerseusPageView.js
+++ app/views/PerseusPageView.js
@@ -7,28 +7,28 @@ define([
 'models/PerseusPage',
 'views/BookView' ], 
 function( gv, PerseusPage, BookView ) {
-	var state = gv.state;
-	
-	// View: Perseus Page View
-	
-	return BookView.extend({
-		className: 'perseus-page-view',
-		template: '#perseus-page-template',
-		
-		render: function(){
-			var self = this;
-			if ( 'page' in self ){
-				console.log( 'view--render!' )
-				self.renderTemplate( { out: self.page.out } );
-			}
-		},
-		
-		initialize: function(){
-			var self = this;
-			console.log( 'view--init!' );
-			self.page = new PerseusPage( "alcathoe-bio-1" );
-			console.log( self );
-			self.bindState( self.page.event.success, self.render, self )
-		}
-	});
+  var state = gv.state;
+  
+  // View: Perseus Page View
+  
+  return BookView.extend({
+    className: 'perseus-page-view',
+    template: '#perseus-page-template',
+    
+    render: function(){
+      var self = this;
+      if ( 'page' in self ){
+        console.log( 'view--render!' )
+        self.renderTemplate( { out: self.page.out } );
+      }
+    },
+    
+    initialize: function(){
+      var self = this;
+      console.log( 'view--init!' );
+      self.page = new PerseusPage( "alcathoe-bio-1" );
+      console.log( self );
+      self.bindState( self.page.event.success, self.render, self )
+    }
+  });
 });
